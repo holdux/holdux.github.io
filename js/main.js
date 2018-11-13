3bf25d5f4d9b73a7c59b2b8c4ecf7f0788413f6d
holdux <42838687+holdux@users.noreply.github.com>
2018-11-13 14:52:18 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+<script>
+  // Look for .hamburger
+  var hamburger = document.querySelector(".hamburger");
+  // On click
+  hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+  });
+</script>
