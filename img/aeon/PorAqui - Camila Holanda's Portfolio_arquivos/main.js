95d14a2d8302f9df61d922a96c69b6d499fc69f7
Camila Holanda <holdux@Camilas-MacBook-Pro.local>
2019-02-06 23:56:23 -0300

    PA - LG -Done

---
+++ img/aeon/PorAqui - Camila Holanda's Portfolio_arquivos/main.js
@@ -0,0 +1,55 @@
+// Look for .hamburger
+var hamburger = document.querySelector('.hamburger')
+// On click
+hamburger.addEventListener('click', function () {
+  // Toggle class "is-active"
+  hamburger.classList.toggle('is-active')
+  // Do something else, like open/close menu
+})
+
+// Cocoen
+document.querySelectorAll('.cocoen').forEach(function (element) {
+  new Cocoen(element)
+})
+
+// Timeline
+const links = document.querySelectorAll('.link')
+const allItnes = document.querySelectorAll('.timeline-item')
+
+
+links.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault()
+    links.forEach(item => item.classList.remove('is-selected'))
+    link.classList.add('is-selected')
+    switch (link.textContent.toLowerCase()) {
+      case 'work':
+        controlVisibilityItens('education')
+        break
+      case 'education':
+        controlVisibilityItens('work')
+        break
+      case 'all':
+        controlVisibilityItens('')
+        break
+      default:
+        break
+    }
+  })
+})
+
+function controlVisibilityItens(className) {
+  toggleVisibleItens(className)
+}
+
+function toggleVisibleItens(className) {
+  allItnes.forEach(item => {
+    if (item.classList.contains(className)) {
+      item.classList.remove('show-itens')
+      item.classList.add('hide-itens')
+    } else {
+      item.classList.remove('hide-itens')
+      item.classList.add('show-itens')
+    }
+  })
+}
